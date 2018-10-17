ba9592cfc1f4003f32b5b096147c681e27d4171b
Muhammad Asif <masifofficial0@gmail.com>
2018-10-17 15:52:19 +0500

    Finished Initial Task

--- main/views.py
+++ main/views.py
@@ -1,7 +1,24 @@
 from django.views.generic import FormView
 from .forms import MessageForm
+from .models import Message
 
 
 class HomePageView(FormView):
     template_name = 'main/home.html'
     form_class = MessageForm
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+
+        context['messages'] = Message.objects.all()
+        return context
+
+    def form_valid(self, form):
+        message_content = form.cleaned_data['message_content']
+
+        Message.objects.create(
+            message_content = message_content
+        )
+
+        return super().form_valid(form)
